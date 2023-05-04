280b95e3deb1c41888552bcdfb8b6b265d7304ab
SDgg01 <119867370+SDgg01@users.noreply.github.com>
2023-05-04 16:56:59 +0800

    Update lp_filter.h

--- rc_common/include/rc_common/filters/lp_filter.h
+++ rc_common/include/rc_common/filters/lp_filter.h
@@ -41,31 +41,32 @@
 #include <rc_msgs/LpData.h>
 #include <realtime_tools/realtime_publisher.h>
 
-class LowPassFilter {
+class LowPassFilter 
+{
 public:
-    explicit LowPassFilter(ros::NodeHandle &nh);
-    explicit LowPassFilter(double cutoff_freq);
-    void input(double in);
-    void input(double in, ros::Time time);
-    double output();
-    void reset();
+  explicit LowPassFilter(ros::NodeHandle& nh);
+  explicit LowPassFilter(double cutoff_freq);
+  void input(double in);
+  void input(double in, ros::Time time);
+  double output();
+  void reset();
 
 private:
-    double in_[3]{};
-    double out_[3]{};
+  double in_[3]{};
+  double out_[3]{};
 
-    // Cutoff frequency for the derivative calculation in Hz.
-    // Negative -> Has not been set by the user yet, so use a default.
-    double cutoff_frequency_ = -1;
-    // Used in filter calculations. Default 1.0 corresponds to a cutoff frequency
-    // at 1/4 of the sample rate.
-    double c_ = 1.;
-    // Used to check for tan(0)==>NaN in the filter calculation
-    double tan_filt_ = 1.;
-    bool is_debug_{};
+  // Cutoff frequency for the derivative calculation in Hz.
+  // Negative -> Has not been set by the user yet, so use a default.
+  double cutoff_frequency_ = -1;
+  // Used in filter calculations. Default 1.0 corresponds to a cutoff frequency
+  // at 1/4 of the sample rate.
+  double c_ = 1.;
+  // Used to check for tan(0)==>NaN in the filter calculation
+  double tan_filt_ = 1.;
+  bool is_debug_{};
 
-    ros::Time prev_time_;
-    ros::Duration delta_t_;
+  ros::Time prev_time_;
+  ros::Duration delta_t_;
 
-    std::shared_ptr<realtime_tools::RealtimePublisher<rc_msgs::LpData>> realtime_pub_{};
+  std::shared_ptr<realtime_tools::RealtimePublisher<rc_msgs::LpData>> realtime_pub_{};
 };
